873baef8e8a6c329588999b46e403581dffa37ba
Benedict <benedictcontawe@gmail.com>
2019-07-06 13:15:19 +0800

    attrs and layout update, MainActivity update

--- app/src/main/java/com/example/customappcompatbutton/MainActivity.kt
+++ app/src/main/java/com/example/customappcompatbutton/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.customappcompatbutton
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btn_hello.setOnClickListener{
-            Log.d(MainActivity::class.java.simpleName,"btn_hello clicked!")
-        }
     }
 }
