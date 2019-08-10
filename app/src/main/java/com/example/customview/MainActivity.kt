34962f55a1e963eb8046dd1bdd444d361295cfd1
Benedict <benedictcontawe@gmail.com>
2019-08-10 14:03:00 +0800

    update main activity

--- app/src/main/java/com/example/customview/MainActivity.kt
+++ app/src/main/java/com/example/customview/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
             }
         )
 
-        customEditText2.setListener(
+        customEditText3.setListener(
             onTextChanged = {
                 Log.d(MainActivity::class.java.simpleName,"onTextChanged")
             },
