a7a785cc85f75a902363ce6af1e29e1e04a6c4a1
Benedict <benedictcontawe@gmail.com>
2020-04-10 21:08:17 +0800

    Update create view in all fragments

--- app/src/main/java/com/example/customviewapp/HomeFragment.java
+++ app/src/main/java/com/example/customviewapp/HomeFragment.java
@@ -1,6 +1,15 @@
 package com.example.customviewapp;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
 }
