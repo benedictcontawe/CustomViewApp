7107e5bcdccd2a238dc69c39e5261e1e3ca88b31
Benedict <benedictcontawe@gmail.com>
2020-04-10 20:44:20 +0800

    Update main class with button click listener and toast

--- app/src/main/java/com/example/customviewapp/MainActivity.java
+++ app/src/main/java/com/example/customviewapp/MainActivity.java
@@ -4,15 +4,19 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private Toolbar toolbar;
     private MenuInflater inflater;
+    private ImageButton btnHome,btnSearch,btnEdit,btnDelete,btnUtilities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,17 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        btnHome = (ImageButton)findViewById(R.id.btnHome);
+        btnSearch = (ImageButton)findViewById(R.id.btnSearch);
+        btnEdit = (ImageButton)findViewById(R.id.btnEdit);
+        btnDelete = (ImageButton)findViewById(R.id.btnDelete);
+        btnUtilities = (ImageButton)findViewById(R.id.btnUtilities);
+
+        btnHome.setOnClickListener(this);
+        btnSearch.setOnClickListener(this);
+        btnEdit.setOnClickListener(this);
+        btnDelete.setOnClickListener(this);
+        btnUtilities.setOnClickListener(this);
     }
 
     @Override
@@ -53,12 +68,28 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnHome:
+                Toast.makeText(this, "btnHome", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnSearch:
+                Toast.makeText(this, "btnSearch", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnEdit:
+                Toast.makeText(this, "btnEdit", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnDelete:
+                Toast.makeText(this, "btnDelete", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnUtilities:
+                Toast.makeText(this, "btnUtilities", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(this, "Default", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
-
-/*
-https://www.youtube.com/watch?v=DMkzIOLppf4
-
-https://www.youtube.com/watch?v=oh4YOj9VkVE
-
-https://www.youtube.com/watch?v=A9rcKZUm0zM
- */
