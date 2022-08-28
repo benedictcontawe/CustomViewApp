408a1287ea0499ad8ca4a0939ab641598780bd32
Benedict Contawe <benedict.contawe@afpayments.com>
2022-08-28 21:30:28 +0800

    Add Chip Programmatically

--- app/src/main/java/com/example/customviewapp/MainActivity.java
+++ app/src/main/java/com/example/customviewapp/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.customviewapp;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipDrawable;
 import com.google.android.material.chip.ChipGroup;
 
 import java.util.List;
@@ -42,12 +45,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         adapter = new ViewPagerAdapter(getSupportFragmentManager(), getLifecycle(), viewModel.getViewPagerList());
         viewPager.setAdapter(adapter);
         viewPager.setUserInputEnabled(true);
+
+        chipGroup.removeAllViews();
+        chipGroup.addView(chipHome);
+        chipGroup.addView(chipSearch);
+        chipGroup.addView(chopEdit);
+        chipGroup.addView(chipDelete);
+        chipGroup.addView(chipUtilities);
+        chipGroup.addView(chipClose);
+
+        Chip chip = new Chip(new ContextThemeWrapper(this,R.style.CustomChipFilterStyle));
+        chip.setId(ViewCompat.generateViewId());
+        chip.setCheckable(true);
+        chip.setChipBackgroundColorResource(R.color.selector_chip_background);
+        chip.setCloseIconVisible(true);
+        chip.setText("ABCDEF");
+        chip.setTextAppearanceResource(R.style.CustomChipFilterStyle);
+        chipGroup.addView(chip);
     }
 
     @Override
     public void onClick(View view) {
         if (chipClose == view) {
             Log.d(TAG,"Chip close");
+            chipGroup.removeView(view);
         }
     }
 
